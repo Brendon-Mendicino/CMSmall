94503d4e3e30b21806574aa2d1398c4562db304d
Brendon Mendicino <brendonmendicino@yahoo.it>
2023-06-21 17:09:38 +0200

    fixed 404page

--- client/src/components/Page404.jsx
+++ client/src/components/Page404.jsx
@@ -4,7 +4,7 @@ import { IconAirplane } from "../assets/icons";
 
 export default function Page404() {
   return (
-    <Container>
+    <Container style={{ minHeight: "100vh", width: "100vw", paddingTop: "22rem", overflow: "hidden", marginTop: "-11rem"}}>
       <Row sm={1} md={2} className="d-flex">
         <Col>
           <h1>404 Page Not Found!</h1>
@@ -14,9 +14,9 @@ export default function Page404() {
           <IconAirplane width={50} height={50} />
         </Col>
       </Row>
-      <Row style={{ marginTop: "100px" }}>
+      <Row style={{ marginTop: "100px", position: "relative" }}>
         <Col className="moving">
-          <IconAirplane width={50} height={50} style={{ rotate: "-90deg" }} />
+          <IconAirplane width={50} height={50} style={{ rotate: "-90deg"}} className="step-rotate" />
         </Col>
       </Row>
     </Container>
